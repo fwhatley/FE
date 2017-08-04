da451aba26392b6b1ae0f1f514e8025bc8d4c3c4
Fredy <fredywhatley@gmail.com>
2017-08-03 21:42:53 -0400

    base with lite-server with automatic browser reload

---
+++ README.md
@@ -0,0 +1,3 @@
+light-server: https://www.npmjs.com/package/light-server
+
+Serve with livereload: npm start: http://localhost:7000
---
+++ lite-server.config.js
@@ -0,0 +1,11 @@
+{
+    "port": 7000,
+    "serve": "./",
+    "watchexps": [
+        "*.html",
+        "*.js",
+        "*.css"
+    ],
+    "open": true
+}
+
